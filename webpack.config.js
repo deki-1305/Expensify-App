672673a9d4ac0afd6d18ac4b2feeef987b61466d
deki-1305 <dejanpelagic@gmail.com>
2021-09-22 12:20:08 +0200

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); //dodavanje plugina za extrak.
 
-module.exports = {
+module.exports = (env) => {
+    isProduction = env === 'production'; // kada je production onda koristimo isProducton
+    const CSSExtract = new ExtractTextPlugin('styles.css'); //kreiramo novu instancu plugina
+    //ime instance CSSExtract je proizvoljno, a fajl css ce biti taj u koji sve smestamo kasnije
+    return {
     entry: './src/app.js',
     output: {
         path: path.join(__dirname, 'public'),
@@ -13,16 +18,23 @@ module.exports = {
             exclude: /node_modules/
         }, {
             test: /\.s?css$/,
-            use: [
-                'style-loader', 
-                'css-loader',
-                'sass-loader'
+            // use: ['style-loader', 'css-loader', 'sass-loader'] pre optimizacije
+            // use: CSSExtract.extract({use: ['css-loader', 'sass-loader']}) //nakon optimizacije
+            //styles idu u odvojeni fajl
+            use: CSSExtract.extract({use: [
+                {loader: 'css-loader',
+                options: {sourceMap: true} },
+                {loader: 'sass-loader',
+                options: {sourceMap: true} }
             ]
+            })
         } ]
     },
-    devtool: 'cheap-module-eval-source-map',
+    plugins: [ CSSExtract ],  // dodali smo ovo
+    devtool: isProduction ? 'source-map' : 'inline-source-map', //zadnja samo za development
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true
     }
+    };
 };
